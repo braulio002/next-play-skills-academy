983fc2b9d156d922c5986602554eb6e356fd1d2f
braulio <braulio.rubio20@gmail.com>
2025-04-26 23:54:07 -0500

    Temporarily replaced site with down page

---
+++ script.js
@@ -0,0 +1,82 @@
+function toggleMenu() {
+    const mobileMenu = document.querySelector(".mobile-menu");
+    const hamburgerIcon = document.querySelector(".hamburger-icon");
+
+    mobileMenu.classList.toggle("active");
+    hamburgerIcon.classList.toggle("active");
+}
+
+const themeToggle = document.getElementById("theme-toggle");
+const body = document.body;
+const icon = themeToggle.querySelector("i");
+
+if (body.classList.contains("dark-mode")) {
+    icon.classList.replace("ri-moon-fill", "ri-sun-fill");
+}
+
+themeToggle.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+
+    if (body.classList.contains("dark-mode")) {
+        icon.classList.replace("ri-moon-fill", "ri-sun-fill");
+    } else {
+        icon.classList.replace("ri-sun-fill", "ri-moon-fill");
+    }
+});
+
+// Videos
+const videos = document.querySelectorAll('video');
+
+videos.forEach(video => {
+  video.muted = true;
+  video.volume = 0;
+
+  video.addEventListener('volumechange', () => {
+    video.muted = true;
+    video.volume = 0;
+  });
+
+  video.addEventListener('click', () => {
+    if (video.paused) {
+      video.play();
+    } else {
+      video.pause();
+    }
+    
+    video.muted = true;
+    video.volume = 0;
+  });
+  
+  video.addEventListener('ended', () => {
+    video.currentTime = 0;
+    video.play();
+  });
+});
+
+// Splash Screen
+window.addEventListener('load', function() {
+    const splashScreen = document.getElementById('splash-screen');
+    
+    setTimeout(function() {
+      splashScreen.style.animation = 'fadeOut 1s ease-out forwards';
+      
+      setTimeout(function() {
+        splashScreen.remove();
+      }, 1000);
+    }, 4500);
+  });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const user = "nextplayskillsacademy";
+  const domain = "gmail.com";
+  const emailLink = document.createElement("a");
+  emailLink.href = `mailto:${user}@${domain}`;
+  emailLink.innerHTML = '<i class="ri-mail-line"></i>';
+  document.getElementById("email-icon").replaceWith(emailLink);
+  
+  const phoneNumber = "+19565371414";
+  const phoneLink = document.createElement("a");
+  phoneLink.href = `tel:${phoneNumber}`;
+  phoneLink.innerHTML = '<i class="ri-phone-line"></i>';
+  document.getElementById("phone-icon").replaceWith(phoneLink);
+});
